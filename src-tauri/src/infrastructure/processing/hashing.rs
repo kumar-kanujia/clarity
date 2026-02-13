aba919cd0853297bea56d00dd6dd65e060fdb817
Kumar <70059467+kumar-kanujia@users.noreply.github.com>
2026-02-13 18:18:10 +0530

    updated: hasing function

--- src-tauri/src/infrastructure/processing/hashing.rs
+++ src-tauri/src/infrastructure/processing/hashing.rs
@@ -1,14 +1,15 @@
 use crate::infrastructure::processing::error::ProcessingError;
 
-use std::{fs::File, path::Path};
+use std::{fs::File, io::BufReader, path::Path};
 
-const PARALLEL_THRESHOLD: i64 = 1024 * 1024;
+const PARALLEL_THRESHOLD_SIZE: i64 = 24 * 1024 * 1024;
+const FILE_BUFFER_CAPACITY: usize = 128 * 1024;
 
 pub fn generate_file_hash<P: AsRef<Path>>(
   path: P,
   file_size: i64,
 ) -> Result<String, ProcessingError> {
-  if file_size > PARALLEL_THRESHOLD {
+  if file_size > PARALLEL_THRESHOLD_SIZE {
     let mut hasher = blake3::Hasher::new();
 
     hasher.update_mmap_rayon(&path)?;
@@ -17,11 +18,12 @@ pub fn generate_file_hash<P: AsRef<Path>>(
 
     Ok(hash.to_hex().to_string())
   } else {
-    let mut file = File::open(path)?;
+    let file = File::open(path)?;
 
+    let mut reader = BufReader::with_capacity(FILE_BUFFER_CAPACITY, file);
     let mut hasher = blake3::Hasher::new();
 
-    std::io::copy(&mut file, &mut hasher)?;
+    std::io::copy(&mut reader, &mut hasher)?;
 
     Ok(hasher.finalize().to_hex().to_string())
   }
