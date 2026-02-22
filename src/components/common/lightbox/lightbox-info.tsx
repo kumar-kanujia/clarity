177788036d1663556145402552a75267649c12ed
Kumar <70059467+kumar-kanujia@users.noreply.github.com>
2026-02-23 00:48:15 +0530

    fix: added tag option lightbox

--- src/components/common/lightbox/lightbox-info.tsx
+++ src/components/common/lightbox/lightbox-info.tsx
@@ -1,4 +1,5 @@
-import { Loader2, Calendar, Monitor, HardDrive } from "lucide-react"
+import { useQuery } from "@tanstack/react-query"
+import { Loader2, Calendar, Monitor, HardDrive, BoxIcon } from "lucide-react"
 
 import {
   Sheet,
@@ -8,8 +9,125 @@ import {
   SheetTrigger
 } from "@/components/ui/sheet"
 import type { ImageItem } from "@/services/tauri"
-import { useQuery } from "@tanstack/react-query"
-import { getAttachedTagsQueryOptions } from "@/features/tags/hooks"
+
+import {
+  getAttachedTagsQueryOptions,
+  getAvailableTagsQueryOptions,
+  useToggleTag
+} from "@/features/tags/hooks"
+
+const TagAction = ({
+  imageId,
+  isSheetOpen
+}: {
+  imageId: number
+  isSheetOpen: boolean
+}) => {
+  const { data, isLoading, isSuccess } = useQuery({
+    ...getAvailableTagsQueryOptions(imageId, 50),
+    enabled: isSheetOpen
+  })
+
+  const { mutate, isPending } = useToggleTag()
+
+  return (
+    <div className="space-y-8">
+      <div className="space-y-3">
+        <h3 className="text-xs font-bold text-zinc-500 uppercase tracking-widest">
+          Add new tag
+        </h3>
+        <div className="bg-zinc-900/50 rounded-xl p-4 border border-zinc-800/50 flex flex-col gap-4">
+          <div className="flex flex-wrap gap-2">
+            {isLoading && (
+              <div className="flex items-center justify-center w-full">
+                <Loader2 className="w-5 h-5 text-zinc-500 animate-spin" />
+              </div>
+            )}
+            {!isLoading && isSuccess && data && data.length > 0 ? (
+              data.map((tag) => (
+                <button
+                  key={tag.id}
+                  onClick={(e) => {
+                    e.preventDefault()
+                    mutate({ imageId, tagId: tag.id })
+                  }}
+                  className="px-3 py-1.5 rounded-full text-xs font-medium border transition-colors cursor-default  hover:opacity-80"
+                  style={{
+                    borderColor: `${tag.tagColor}40`,
+                    color: tag.tagColor,
+                    backgroundColor: `${tag.tagColor}1A`
+                  }}
+                  disabled={isPending}
+                >
+                  {tag.tagName}
+                </button>
+              ))
+            ) : (
+              <p className="flex items-center justify-center w-full gap-x-2 text-sm">
+                <BoxIcon className="size-4 text-zinc-500" /> No more tags
+                available
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const AttachedTags = ({
+  imageId,
+  isSheetOpen
+}: {
+  imageId: number
+  isSheetOpen: boolean
+}) => {
+  const { data: attachedTags, isLoading: isTagsLoading } = useQuery({
+    ...getAttachedTagsQueryOptions(imageId, 50),
+    enabled: isSheetOpen
+  })
+
+  const { mutate } = useToggleTag()
+
+  return (
+    <div className="space-y-3">
+      <h3 className="text-xs font-bold text-zinc-500 uppercase tracking-widest flex justify-between items-center">
+        Tags
+      </h3>
+      <div className="bg-zinc-900/50 rounded-xl p-4 border border-zinc-800/50 flex flex-col gap-4">
+        {isTagsLoading ? (
+          <div className="flex items-center justify-center py-4">
+            <Loader2 className="w-5 h-5 text-zinc-500 animate-spin" />
+          </div>
+        ) : attachedTags && attachedTags.length > 0 ? (
+          <div className="flex flex-wrap gap-2">
+            {attachedTags.map((tag) => (
+              <button
+                key={tag.id}
+                onClick={(e) => {
+                  e.preventDefault()
+                  mutate({ imageId, tagId: tag.id })
+                }}
+                className="px-3 py-1.5 rounded-full text-xs font-medium border transition-colors cursor-default hover:opacity-80"
+                style={{
+                  borderColor: `${tag.tagColor}40`,
+                  color: tag.tagColor,
+                  backgroundColor: `${tag.tagColor}1A`
+                }}
+              >
+                {tag.tagName}
+              </button>
+            ))}
+          </div>
+        ) : (
+          <p className="text-sm text-zinc-500 text-center py-2">
+            No tags attached.
+          </p>
+        )}
+      </div>
+    </div>
+  )
+}
 
 interface LightboxInfoProps {
   image: ImageItem
@@ -24,11 +142,6 @@ export const LightboxInfo = ({
   isSheetOpen,
   children
 }: LightboxInfoProps) => {
-  const { data: attachedTags, isLoading: isTagsLoading } = useQuery({
-    ...getAttachedTagsQueryOptions(image.id, 50),
-    enabled: isSheetOpen
-  })
-
   return (
     <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
       <SheetTrigger render={children} />
@@ -73,38 +186,8 @@ export const LightboxInfo = ({
               </div>
             </div>
           </div>
-          <div className="space-y-3">
-            <h3 className="text-xs font-bold text-zinc-500 uppercase tracking-widest flex justify-between items-center">
-              Tags
-            </h3>
-            <div className="bg-zinc-900/50 rounded-xl p-4 border border-zinc-800/50 flex flex-col gap-4">
-              {isTagsLoading ? (
-                <div className="flex items-center justify-center py-4">
-                  <Loader2 className="w-5 h-5 text-zinc-500 animate-spin" />
-                </div>
-              ) : attachedTags && attachedTags.length > 0 ? (
-                <div className="flex flex-wrap gap-2">
-                  {attachedTags.map((tag) => (
-                    <span
-                      key={tag.id}
-                      className="px-3 py-1.5 rounded-full text-xs font-medium border transition-colors cursor-default"
-                      style={{
-                        borderColor: `${tag.tagColor}40`,
-                        color: tag.tagColor,
-                        backgroundColor: `${tag.tagColor}1A`
-                      }}
-                    >
-                      {tag.tagName}
-                    </span>
-                  ))}
-                </div>
-              ) : (
-                <p className="text-sm text-zinc-500 text-center py-2">
-                  No tags attached.
-                </p>
-              )}
-            </div>
-          </div>
+          <AttachedTags imageId={image.id} isSheetOpen={isSheetOpen} />
+          <TagAction imageId={image.id} isSheetOpen={isSheetOpen} />
         </div>
       </SheetContent>
     </Sheet>
